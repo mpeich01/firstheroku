dd660f08be6f295b171fec9984429a50411a0b4a
Max Peicher <maxpeicher@Maxs-MacBook-Pro-5.local>
2020-04-24 21:24:58 -0400

    hello

--- test.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-var url = require('url')
-
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
-}).listen(8080);
